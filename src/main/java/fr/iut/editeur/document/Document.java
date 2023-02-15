795b670a4d7d85fe9e6dd9a8e44170a091640041
Sareen00 <1rokvar1@gmail.com>
2023-02-15 10:10:23 +0100

    ajout de la classe CommandeMajuscules
    modification de plein de fichier

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -20,6 +20,20 @@ public class Document {
         this.texte += texte;
     }
 
+    public void remplacer(int start, int end, String remplacement) {
+        String leftPart = this.texte.substring(0, start);
+        String rightPart = this.texte.substring(end);
+        this.texte = leftPart + remplacement + rightPart;
+    }
+
+    public void majuscules(int start, int end) {
+        String partieMajuscule = this.texte.substring(start, end);
+        partieMajuscule = partieMajuscule.toUpperCase();
+
+        remplacer(start,end,partieMajuscule);
+    }
+
+
     @Override
     public String toString() {
         return this.texte;
